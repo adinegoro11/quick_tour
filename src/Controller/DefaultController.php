ea2f95ff2800cbf30802a92eeb99c623055c4e79
DESKTOP-erlangga <adinegoro11@gmail.com>
2021-05-05 17:17:07 +0700

    Create BlogController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,24 +1,18 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    public function index($name)
-    {
-        // return new Response('Hello! $name');
-        return $this->render('default/index.html.twig', [
-            'name' => $name,
-        ]);
-    }
-
     /**
-        * @Route("/simplicity")
-        */
-    // public function simple()
-    // {
-    //     return new Response('Simple! Easy! Great!');
-    // }
+     * @Route("/hello/{name}")
+     */
+    public function index($name, LoggerInterface $logger)
+    {
+        $logger->info("Saying hello to $name!");
+        // ...
+    }
 }
